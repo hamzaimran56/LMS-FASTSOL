8f4b1997b3d6bc54bad9981e3854f0a838862c21
hamzaimran56 <hmzaimran56@gmail.com>
2026-08-14 21:26:07 +0500

    Fix Course model import case

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -2,7 +2,7 @@ import { Webhook } from "svix";
 import User from "../models/User.js";
 import Stripe from "stripe";
 import { Purchase } from "../models/Purchase.js";
-import Course from "../models/Course.js";
+import Course from "../models/course.js";
 
 // API Controller Function to Manage Clerk User with database
 export const clerkWebhooks = async (req, res) => {
